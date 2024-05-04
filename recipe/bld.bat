30bf1e931735ab6292772e46c698b343dd9f6115
Oliver Borchert <me@borchero.com>
2024-05-04 14:25:57 +0200

    Fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,14 @@
 @echo on
 
-cd release/cmd/tailscale
+cd %SRC_DIR%/git
+for /f "delims=" %%a IN ('git rev-parse HEAD') do set GIT_COMMIT_SHA=%%a
+set "GIT_SHORT_SHA=!GIT_COMMIT_SHA:~0,9!"
+
+cd %SRC_DIR%/release/cmd/tailscale
 
 go-licenses save . ^
     --save_path ../../library_licenses
 
-set "GIT_SHORT_HASH=!GIT_FULL_HASH:~0,9!"
 go install -v ^
-    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=v%PKG_VERSION%' -X 'tailscale.com/version.longStamp=%PKG_VERSION%-t%GIT_SHORT_HASH%' -X 'tailscale.com/version.gitCommitStamp=%GIT_FULL_HASH%'" ^
+    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=v%PKG_VERSION%' -X 'tailscale.com/version.longStamp=%PKG_VERSION%-t%GIT_SHORT_SHA%' -X 'tailscale.com/version.gitCommitStamp=%GIT_COMMIT_SHA%'" ^
     .
