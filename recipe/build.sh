c9632e0c71c6dd17779dcfe8f6a9069b8d7c8fca
Oliver Borchert <me@borchero.com>
2024-05-04 14:04:04 +0200

    Try fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cd release/cmd/tailscale
 go-licenses save . \
     --save_path ../../library_licenses
 
-GIT_COMMIT_SHA=$(cd ../git && git rev-parse HEAD)
+GIT_COMMIT_SHA=$(cd $SRC_DIR/git && git rev-parse HEAD)
 GIT_SHORT_SHA=${GIT_FULL_HASH:0:9}
 go build -v \
     -ldflags "-s -w -X 'tailscale.com/version.shortStamp=$PKG_VERSION' -X 'tailscale.com/version.longStamp=$PKG_VERSION-t$GIT_SHORT_SHA' -X 'tailscale.com/version.gitCommitStamp=$GIT_COMMIT_SHA'" \
