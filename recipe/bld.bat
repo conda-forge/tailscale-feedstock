96c2167f53887e5d7ffbb9db48e60158a7293e6a
Oliver Borchert <me@borchero.com>
2024-05-04 13:16:51 +0200

    Add commit SHA to version information

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,11 @@
 @echo on
 
-cd cmd/tailscale
+cd release/cmd/tailscale
 
 go-licenses save . ^
     --save_path ../../library_licenses
 
+set "GIT_SHORT_HASH=!GIT_FULL_HASH:~0,9!"
 go install -v ^
-    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=v%PKG_VERSION%'" ^
+    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=v%PKG_VERSION%' -X 'tailscale.com/version.longStamp=%PKG_VERSION%-t%GIT_SHORT_HASH%' -X 'tailscale.com/version.gitCommitStamp=%GIT_FULL_HASH%'" ^
     .
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cd cmd/tailscale
+cd release/cmd/tailscale
 
 go-licenses save . \
     --save_path ../../library_licenses
 
+GIT_SHORT_HASH=${GIT_FULL_HASH:0:9}
 go build -v \
-    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=$PKG_VERSION'" \
+    -ldflags "-s -w -X 'tailscale.com/version.shortStamp=$PKG_VERSION' -X 'tailscale.com/version.longStamp=$PKG_VERSION-t$GIT_SHORT_HASH' -X 'tailscale.com/version.gitCommitStamp=$GIT_FULL_HASH'" \
     -o $PREFIX/bin/tailscale
